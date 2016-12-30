98e026e15850da3350a9bf610864ff72fb9f307e
RNemykin <nemykinroman@gmail.com>
2016-12-30 15:50:41 +0300

    init

---
+++ src/main/java/ru/ho4upizzu/Ho4uPizzuApplication.java
@@ -0,0 +1,12 @@
+package ru.ho4upizzu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ho4uPizzuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ho4uPizzuApplication.class, args);
+	}
+}
