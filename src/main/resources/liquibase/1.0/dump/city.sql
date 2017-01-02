e7efe9f3b504bd7b84c1f229a2fe13d18e754a77
RNemykin <nemykinroman@gmail.com>
2017-01-02 13:23:55 +0300

    db

--- src/main/resources/liquibase/1.0/dump/city.sql
+++ src/main/resources/liquibase/1.0/dump/city.sql
@@ -1,1 +1,2 @@
-INSERT INTO city (id, name) VALUES (1, 'Белгород');
+INSERT INTO city (id, name, type) VALUES (1, 'Белгород', '');
+INSERT INTO city (id, name, type) VALUES (2, 'Разумное', 'пос. ');
