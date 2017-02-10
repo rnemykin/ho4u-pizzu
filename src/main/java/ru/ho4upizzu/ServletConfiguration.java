f4bda4dafb5cbbb26bb9d9d139539d5040db156d
RNemykin <nemykinroman@gmail.com>
2017-02-10 23:10:47 +0300

    ~

--- src/main/java/ru/ho4upizzu/ServletConfiguration.java
+++ src/main/java/ru/ho4upizzu/ServletConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("prod")
+@Profile("open-shift")
 public class ServletConfiguration extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
